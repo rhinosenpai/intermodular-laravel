22e185f7c094dae5ba66e4c52d9d4e053deaddae
Trench <crodg1023@outlook.com>
2024-02-07 20:42:41 +0100

    Cositas de la BD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(FormularioSeeder::class);
         $this->call(PreguntaSeeder::class);
         $this->call(Formulario_PreguntaSeeder::class);
+        $this->call(UsuariosSeeder::class);
     }
 }
