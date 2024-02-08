22d125af8796b5e4d609de77800801aadc966b12
Ismael <ismaelauam@gmail.com>
2024-02-08 18:29:38 +0100

    Añadido el controlador centros de la api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/centros', 'App\Http\Controllers\Api\CentroController@index'); //mostrar todos los registros de centros
+Route::get('/centros/{id}', 'App\Http\Controllers\Api\CentroController@show'); //mostrar un registro especifico
+Route::post('/centros', 'App\Http\Controllers\Api\CentroController@store'); //crear un registro
+Route::put('/centros/{id}', 'App\Http\Controllers\Api\CentroController@update'); //actualizar un registro
+Route::delete('/centros/{id}', 'App\Http\Controllers\Api\CentroController@destroy'); //eliminar un registro
