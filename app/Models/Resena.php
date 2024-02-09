7da46016ee5bc0161d82ea54b66db251bf465650
Pablo Mata <matacastello@gmail.com>
2024-02-09 13:07:55 +0100

    Eliminar tabla token, editar tabla reseña

--- app/Models/Resena.php
+++ app/Models/Resena.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Resena extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $table = 'resena';
 }
