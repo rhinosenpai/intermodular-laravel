9f69dff97c2f1b28ac32c02ed7d21bfcb673637e
Ismael <ismaelauam@gmail.com>
2024-02-18 01:47:43 +0100

    add empresa imagen

--- database/migrations/2024_02_05_184105_create_empresas_table.php
+++ database/migrations/2024_02_05_184105_create_empresas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('empresas', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('imagen');
+            $table->longText('imagen');
             $table->integer('nota');
             $table->string('cif');
             $table->string('descripcion');
